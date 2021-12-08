42a12adee894d7eedbd41f221dd63b7d1d8340eb
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-12-08 15:23:52 +0300

    Add license and prepare initial page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-ngrx-usage';
+  public title = `Angular ${VERSION.major} NGRX Usage`;
 }
